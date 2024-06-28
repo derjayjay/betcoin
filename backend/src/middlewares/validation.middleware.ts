fa022480a512f3f50cf49f31ec34bd3d45a28ef7
Julian J. Maurer <mail@julianjmaurer.de>
2024-06-28 14:37:32 +0200

    Backend: Validation middleware does no longer give detailed information when running in production

--- backend/src/middlewares/validation.middleware.ts
+++ backend/src/middlewares/validation.middleware.ts
@@ -3,24 +3,37 @@ import { StatusCodes } from 'http-status-codes';
 import Joi from 'joi';
 
 import { logger } from '../app';
+import config from '../utils/config';
 
+/**
+ * Validates the request body against the provided schema.
+ * If the validation fails, it sends a response with a status code BAD_REQUEST.
+ *
+ * @param schema - The Joi schema used for validation.
+ */
 export const validateBody: (schema: Joi.ObjectSchema) => RequestHandler = (schema: Joi.ObjectSchema) => {
   return (req: Request, res: Response, next: NextFunction) => {
     const { error } = schema.validate(req.body);
     if (error) {
       logger.debug(error, 'Validating request body failed');
-      return res.status(StatusCodes.BAD_REQUEST).json({ error: error.details[0].message });
+      return res.status(StatusCodes.BAD_REQUEST).json(config.isProduction ? {} : { error: error.details[0].message });
     }
     next();
   };
 };
 
+/**
+ * Validates the request parameters against the provided schema.
+ * If the validation fails, it sends a response with a status code BAD_REQUEST.
+ *
+ * @param schema - The Joi schema used for validation.
+ */
 export const validateParameters: (schema: Joi.ObjectSchema) => RequestHandler = (schema: Joi.ObjectSchema) => {
   return (req: Request, res: Response, next: NextFunction) => {
     const { error } = schema.validate(req.params);
     if (error) {
       logger.debug(error, 'Validating request parameters failed');
-      return res.status(StatusCodes.BAD_REQUEST).json({ error: error.details[0].message });
+      return res.status(StatusCodes.BAD_REQUEST).json(config.isProduction ? {} : { error: error.details[0].message });
     }
     next();
   };
